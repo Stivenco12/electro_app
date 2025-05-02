78ba02028a41ec69d1d8bd51b989edd668589185
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-02 14:25:13 -0500

    feat: :tada: Initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/main/java/electro_app/electro_app/ElectroAppApplication.java
@@ -0,0 +1,13 @@
+package electro_app.electro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroAppApplication.class, args);
+	}
+
+}
