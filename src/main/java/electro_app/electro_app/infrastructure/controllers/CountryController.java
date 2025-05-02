8d7f2da9a37838b1196294ba8926c9b9b76dfd8e
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-02 16:59:57 -0500

    feat ✨ Implement CountryController for managing country entities with CRUD operations

---
+++ src/main/java/electro_app/electro_app/infrastructure/controllers/CountryController.java
@@ -0,0 +1,45 @@
+package electro_app.electro_app.infrastructure.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import electro_app.electro_app.application.services.ICountryService;
+import electro_app.electro_app.domain.entities.Country;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/api/country")
+public class CountryController {
+    @Autowired
+    private ICountryService countryService;
+
+    @GetMapping
+    public List<Country> findAll() {
+        return countryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable int id) {
+        Optional<Country> countryOptional = countryService.findById(id);
+        if (countryOptional.isPresent()) {
+            return ResponseEntity.ok(countryOptional.orElseThrow());
+        } 
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        Optional<Country> countryOptional = countryService.delete(id);
+        if (countryOptional.isPresent()) {
+            return ResponseEntity.ok(countryOptional.orElseThrow());
+        } 
+        return ResponseEntity.notFound().build();
+    }
+    
+    
+}
